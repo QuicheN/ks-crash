f455e86309d2937e94af22240972d5f2ef70e2b3
Kishen Sargur <kishensargur@gmail.com>
2026-07-20 18:11:47 -0500

    chore

---
+++ src/rendering/CameraRig.jsx
@@ -0,0 +1,2 @@
+//Implement later to follow the car around and load based on driver perspective instead of 
+//overhead view. This will require a new way to load the scene based on the car's position and orientation.
